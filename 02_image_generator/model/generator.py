91403fe7285a5f7e78bba9beb10a6c8511aa859e
Jagrav <jagravsinghjs@gmail.com>
2026-08-24 22:13:46 +0530

    completed generator.py

---
+++ 02_image_generator/model/generator.py
@@ -0,0 +1,19 @@
+from model.load_model import load_model
+import torch
+
+pipe, device = load_model()
+
+
+def generate_image(prompt: str, negative_prompt: str = "", steps: int = 20, guidance_scale: float = 7.5):
+    generator = torch.Generator(device=device).manual_seed(torch.seed())
+
+    with torch.inference_mode():
+        image = pipe(
+            prompt=prompt,
+            negative_prompt="negative_prompt",
+            num_inference_steps=steps,
+            guidance_scale=7,
+            generator=generator,
+        ).images[0]
+
+    return image
