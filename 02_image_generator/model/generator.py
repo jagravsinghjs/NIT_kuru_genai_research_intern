1447a654cdce504ff09bae1db12570ece6bbee00
Jagrav <jagravsinghjs@gmail.com>
2026-08-25 02:02:05 +0530

    changed steps to 40 and guidance scale to 9. also added negative prompt

--- 02_image_generator/model/generator.py
+++ 02_image_generator/model/generator.py
@@ -4,15 +4,15 @@ import torch
 pipe, device = load_model()
 
 
-def generate_image(prompt: str, negative_prompt: str = "", steps: int = 20, guidance_scale: float = 7.5):
+def generate_image(prompt: str, negative_prompt: str = "", steps: int = 40, guidance_scale: float = 7.5):
     generator = torch.Generator(device=device).manual_seed(torch.seed())
 
     with torch.inference_mode():
         image = pipe(
             prompt=prompt,
-            negative_prompt="negative_prompt",
+            negative_prompt="blurry, low quality, deformed, disfigured, bad anatomy, extra limbs, watermark, text, ugly",
             num_inference_steps=steps,
-            guidance_scale=7,
+            guidance_scale=9,
             generator=generator,
         ).images[0]
 
