a30791da9262f27d8ac9b911566841cb3585688c
Jagrav <jagravsinghjs@gmail.com>
2026-08-28 00:17:50 +0530

    completed generator.py

---
+++ 03_image_captioning/model/generator.py
@@ -0,0 +1,17 @@
+from model.load_model import load_model
+from PIL import Image
+import torch
+
+processor, model, device = load_model()
+
+
+def generate_caption(image_path: str):
+    image = Image.open(image_path).convert("RGB")
+
+    inputs = processor(images=image, return_tensors="pt").to(device)
+
+    with torch.inference_mode():
+        output_ids = model.generate(**inputs, max_new_tokens=50)
+
+    caption = processor.decode(output_ids[0], skip_special_tokens=True)
+    return caption
