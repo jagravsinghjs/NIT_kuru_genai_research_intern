da8d999331ae01711bfb4c2f91751063367a99de
Jagrav <jagravsinghjs@gmail.com>
2026-09-01 22:16:51 +0530

    changed model to Qwen/Qwen2-VL-7B-Instruct, edited load_model.py

--- 03_image_captioning/model/load_model.py
+++ 03_image_captioning/model/load_model.py
@@ -1,17 +1,25 @@
-from transformers import BlipProcessor, BlipForConditionalGeneration
+from transformers import Qwen2VLForConditionalGeneration, AutoProcessor, BitsAndBytesConfig
 import torch
 
-MODEL_NAME = "Salesforce/blip-image-captioning-base"
+MODEL_NAME = "Qwen/Qwen2-VL-7B-Instruct"
 
 
 def load_model():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    processor = BlipProcessor.from_pretrained(MODEL_NAME)
-    model = BlipForConditionalGeneration.from_pretrained(MODEL_NAME).to(device)
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_compute_dtype=torch.float16,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_use_double_quant=True,
+    )
+
+    processor = AutoProcessor.from_pretrained(MODEL_NAME)
+    model = Qwen2VLForConditionalGeneration.from_pretrained(
+        MODEL_NAME,
+        quantization_config=bnb_config,
+        device_map="auto",
+    )
     model.eval()
 
     return processor, model, device
-if __name__ == "__main__":
-    processor, model, device = load_model()
-    print("Model loaded successfully on", device)
